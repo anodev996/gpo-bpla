3330ef8043ac39b83e396801d764ef1a12996c43
Ushacow Maxim <max.201uchacow@gmail.com>
2026-05-28 21:22:55 +0700

    Add Terrain

---
+++ unity/Assets/Scripts/Input/PointInput.cs
@@ -0,0 +1,30 @@
+﻿
+using UnityEngine;
+
+/// <summary>
+/// Мусорный класс под небольшую анимацию перемещения
+/// </summary>
+public class PointInput : MonoBehaviour
+{
+    [SerializeField] private ControlledBody controlled;
+    [SerializeField] private Vector3 point;
+    [SerializeField] private float angle;
+
+    public void FixedUpdate()
+    {
+        Vector3 toPoint = point - controlled.transform.position;
+
+        if (toPoint.sqrMagnitude < 0.01f)
+        {
+            controlled.direction = Vector3.zero;
+        }
+        else
+        {
+            float x = toPoint.x;
+            float z = toPoint.z;
+            toPoint.x = z;
+            toPoint.z = x;
+            controlled.direction = Vector3.Lerp(controlled.direction, toPoint.normalized, Time.fixedDeltaTime);
+        }
+    }
+}
